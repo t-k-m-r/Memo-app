f629dcc1a2e098c41eb17a6067d9dd5d472a97af
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2024-01-09 18:31:41 +0900

    詳細ページ&リンク作成

--- src/main/java/com/example/memo/domain/memo/MemoRepository.java
+++ src/main/java/com/example/memo/domain/memo/MemoRepository.java
@@ -3,6 +3,7 @@ package com.example.memo.domain.memo;
 import com.example.memo.domain.MemoEntity;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -14,4 +15,7 @@ public interface MemoRepository {
 
     @Insert("insert into memos (title, text, edit_time) values (#{title}, #{text}, now())")
     void insert(String title, String text);
+
+    @Select("SELECT id, title, text, edit_time FROM memos WHERE id = #{memoId}")
+    MemoEntity findById(@Param("memoId") long id);
 }
