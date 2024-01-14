c09d4cc304b0af1e80ec6eeb13474c1d29a8bc70
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2024-01-14 14:59:34 +0900

    H2からPostgreSQLに変更

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table memos (
-id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id SERIAL PRIMARY KEY,
 title VARCHAR(50),
 text TEXT NOT NULL,
-edit_time DATETIME NOT NULL
+edit_time TIMESTAMP NOT NULL
 );
